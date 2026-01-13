a3cc772305b8b7e14150f55113d7b3d1c3001750
arnau-guell <guell@ub.edu>
2026-01-13 18:54:50 +0100

    Heuristic configuration v1

---
+++ 1-convert/heuristic_generacion.py
@@ -0,0 +1,82 @@
+############################################
+#######  GENERACION HEURISTIC FILE   #######
+#######       PENLab Jan 2026        #######
+############################################
+
+import os
+
+grouping = 'all'                   # change it to 'all' if Study Identifier UID error appears
+
+delete_scans = True
+delete_events = True
+
+# create files function
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
+    if template is None or not template:
+        raise ValueError('Template must be a valid format string')
+    return template, outtype, annotation_classes
+
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+
+    allowed template fields - follow python string module:
+
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+    # paths for the desired output BIDS. Further BIDS paths must follow BIDS
+    # specification filenames: https://bids-standard.github.io/bids-starter-kit/folders_and_files/files.html
+    
+    t1w=create_key('sub-{subject}/anat/sub-{subject}_run-{item:02d}_T1w')
+    
+    rest_bold=create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:02d}_bold')
+    pain_bold=create_key('sub-{subject}/func/sub-{subject}_task-pain_run-{item:02d}_bold')
+    multisens_bold=create_key('sub-{subject}/func/sub-{subject}_task-multisens_run-{item:02d}_bold')
+    selfvowel_bold=create_key('sub-{subject}/func/sub-{subject}_task-SelfVowel_run-{item:02d}_bold')
+    nback_bold=create_key('sub-{subject}/func/sub-{subject}_task-nback_run-{item:02d}_bold')
+
+    # rest_sbref=create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:02d}_sbref') ?
+
+    dwi=create_key('sub-{subject}/dwi/sub-{subject}_run-{item:02d}_dwi')
+
+    # dictionary: list of DICOMs for each BIDS file
+    info =  {
+                    t1w:[],
+                    rest_bold:[],
+                    pain_bold:[],
+                    multisens_bold:[],
+                    selfvowel_bold:[],
+                    nback_bold:[],
+                    dwi:[],
+             }
+
+    # append DICOMS to dictionary keys --> to change in function of protocol_name,
+    # can be found in <bidspath>/.heudiconv/<subject>/ses-<session>/info/dicominfo_ses-<session>.tsv
+    # test heudiconv can be done by 'heudiconv --files <dicoms_path>/<subject>/*/*.IMA -o <bids_path>/ -f convertall -s <subject> -c none' on command
+    
+    for s in seqinfo:
+       
+        #anat
+        if s.protocol_name=='t1_mprage_sag_p3_iso_Munich':
+            info[t1w].append(s.series_id)   
+
+        #func
+        if s.protocol_name=='resting MULTI a-p ADAPTADO' and s.series_description[-6:] != '_SBRef':
+                info[rest_bold].append(s.series_id)
+        elif 'DOLOR' in s.protocol_name and s.series_description[-6:] != '_SBRef':            
+            info[pain_bold].append(s.series_id)
+        elif 'MULTISENSORIAL' in s.protocol_name and s.series_description[-6:] != '_SBRef':            
+            info[multisens_bold].append(s.series_id)
+        elif 'AUTOJUICIO' in s.protocol_name and s.series_description[-6:] != '_SBRef':            
+            info[selfvowel_bold].append(s.series_id)
+        elif 'NBACK' in s.protocol_name and s.series_description[-6:] != '_SBRef':            
+            info[selfvowel_bold].append(s.series_id)
+
+        #dwi
+        if s.protocol_name=='DWI_ FASTnFURIUS AP 2X2X2' and s.series_description[-7:] != '_TRACEW':
+            info[dwi].append(s.series_id)
+
+    return info
