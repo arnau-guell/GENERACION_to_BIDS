44a07a17e7fb5509b364aa11b9c6d5aa9e91c296
arnau-guell <guell@ub.edu>
2026-01-14 15:41:18 +0100

    Heuristic file update Added DWI P-A b0

--- 1-convert/heuristic_generacion.py
+++ 1-convert/heuristic_generacion.py
@@ -28,7 +28,7 @@ def infotodict(seqinfo):
     subindex: sub index within group
     """
     # paths for the desired output BIDS. Further BIDS paths must follow BIDS
-    # specification filenames: https://bids-standard.github.io/bids-starter-kit/folders_and_files/files.html
+    # specification filenames: https://bids.neuroimaging.io/getting_started/folders_and_files/files.html
     
     t1w=create_key('sub-{subject}/anat/sub-{subject}_run-{item:02d}_T1w')
     
@@ -38,9 +38,9 @@ def infotodict(seqinfo):
     selfvowel_bold=create_key('sub-{subject}/func/sub-{subject}_task-SelfVowel_run-{item:02d}_bold')
     nback_bold=create_key('sub-{subject}/func/sub-{subject}_task-nback_run-{item:02d}_bold')
 
-    # rest_sbref=create_key('sub-{subject}/func/sub-{subject}_task-rest_run-{item:02d}_sbref') ?
+    dwi=create_key('sub-{subject}/dwi/sub-{subject}_dir-AP_run-{item:02d}_dwi')
 
-    dwi=create_key('sub-{subject}/dwi/sub-{subject}_run-{item:02d}_dwi')
+    dwi_b0_pa=create_key('sub-{subject}/fmap/sub-{subject}_dir-PA_acq-DWI_run-{item:02d}_epi')
 
     # dictionary: list of DICOMs for each BIDS file
     info =  {
@@ -51,11 +51,12 @@ def infotodict(seqinfo):
                     selfvowel_bold:[],
                     nback_bold:[],
                     dwi:[],
+                    dwi_b0_pa:[],
              }
 
     # append DICOMS to dictionary keys --> to change in function of protocol_name,
     # can be found in <bidspath>/.heudiconv/<subject>/ses-<session>/info/dicominfo_ses-<session>.tsv
-    # test heudiconv can be done by 'heudiconv --files <dicoms_path>/<subject>/*/*.IMA -o <bids_path>/ -f convertall -s <subject> -c none' on command
+    # test heudiconv can be done by 'heudiconv --files <dicoms_path>/<subject>/*/*/*/*/*.dcm -o <bids_path>/ -f convertall -s <subject> -c none -g all' on command
     
     for s in seqinfo:
        
@@ -73,10 +74,20 @@ def infotodict(seqinfo):
         elif 'AUTOJUICIO' in s.protocol_name and s.series_description[-6:] != '_SBRef':            
             info[selfvowel_bold].append(s.series_id)
         elif 'NBACK' in s.protocol_name and s.series_description[-6:] != '_SBRef':            
-            info[selfvowel_bold].append(s.series_id)
+            info[nback_bold].append(s.series_id)
 
         #dwi
         if s.protocol_name=='DWI_ FASTnFURIUS AP 2X2X2' and s.series_description[-7:] != '_TRACEW':
             info[dwi].append(s.series_id)
 
+        #fmap
+        if s.protocol_name=='2_B0_PA':
+            info[dwi_b0_pa].append(s.series_id)     
+
     return info
+
+# Fill the IntendedFor metadata field for fmap files
+POPULATE_INTENDED_FOR_OPTS = {
+        'matching_parameters': ['ImagingVolume', 'Shims'],
+        'criterion': 'Closest'
+}
